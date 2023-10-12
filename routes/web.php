47d314b9869a4a3146d30411e6e4fd8126258764
ilsenlh92 <ilsen92@gmail.com>
2023-10-11 23:35:11 -0400

    agregando la ruta para la internacionalizacion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\App;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +19,14 @@ Route::get('/', function () {
 Route::view('/trailers','pages.trailers')->name('trailers');
 Route::view('/foodtrucks','pages.foodtrucks')->name('foodtrucks');
 Route::view('/about-us','pages.about-us')->name('about-us');
+
+//ruta ofrecida por APP para la Internacionalizacion
+Route::get('/greeting/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'es'])) {
+        abort(400);
+    }
+
+    App::setLocale($locale);
+
+    // ...
+});
