abdd34c5f08a4125e0ea113d8535876961caed56
ilsenlh92 <ilsen92@gmail.com>
2023-10-26 22:44:37 -0400

    efecto shadow al logo

--- resources/views/includes/layout/nav.blade.php
+++ resources/views/includes/layout/nav.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg">
                     <a class="navbar-brand" href="/">
-                        <img src="images/logo.png" alt="Logo Y&Y Window Service Corp">
+                        <img class="logo wsc" src="images/logo.png" alt="Logo Y&Y Window Service Corp">
                         <title id="logo-text"> Window Service Corp </title>
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
